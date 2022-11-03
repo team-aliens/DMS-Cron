bcec74874e90cfb71cacd273746c29d113268069
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-03 20:27:07 +0900

    refactor: (#2) base package 경로 추가

--- src/main/kotlin/team/aliens/dmscron/thirdparty/api/FeignClientConfiguration.kt
+++ src/main/kotlin/team/aliens/dmscron/thirdparty/api/FeignClientConfiguration.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import team.aliens.dmscron.thirdparty.api.error.FeignClientErrorDecoder
 
-@EnableFeignClients(basePackages = ["team.aliens.dmscron"])
+@EnableFeignClients(basePackages = ["team.aliens.dmscron.thirdparty.api"])
 @Configuration
 class FeignClientConfiguration {
 
