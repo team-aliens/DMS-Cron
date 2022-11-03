c767d2558ca1f0670957ec031647044305a03755
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-03 16:29:31 +0900

    init: (#2) project settings

---
+++ src/test/kotlin/team/aliens/dmscron/DmsCronApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.aliens.dmscron
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DmsCronApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
