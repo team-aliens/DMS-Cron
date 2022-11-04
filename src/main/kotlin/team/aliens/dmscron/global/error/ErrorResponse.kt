51158f6bd193c980db1f2406c28e090b50d6923b
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-04 15:40:16 +0900

    refactor: (#2) int -> http status

--- src/main/kotlin/team/aliens/dmscron/global/error/ErrorResponse.kt
+++ src/main/kotlin/team/aliens/dmscron/global/error/ErrorResponse.kt
@@ -1,13 +1,15 @@
 package team.aliens.dmscron.global.error
 
+import org.springframework.http.HttpStatus
+
 data class ErrorResponse(
-    val status: Int,
+    val status: HttpStatus,
     val message: String
 ) {
 
     companion object {
         fun of(errorProperty: ErrorProperty) = ErrorResponse(
-            status = errorProperty.status().value(),
+            status = errorProperty.status(),
             message = errorProperty.message()
         )
     }
