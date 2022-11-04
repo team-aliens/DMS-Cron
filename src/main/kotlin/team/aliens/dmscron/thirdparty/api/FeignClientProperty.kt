1f8ec6335ef2ceed38c6c493f9caa5fa28e0357a
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-05 00:00:24 +0900

    feat: (#4) feign client property

---
+++ src/main/kotlin/team/aliens/dmscron/thirdparty/api/FeignClientProperty.kt
@@ -0,0 +1,11 @@
+package team.aliens.dmscron.thirdparty.api
+
+object FeignClientProperty {
+    const val TYPE = "json"
+    const val PAGE_INDEX = 1
+    const val PAGE_SIZE = 31
+    const val SD_SCHOOL_CODE = "G10"
+    const val SCHOOL_CODE = "7430310"
+    const val STARTED_YMD = "20221024"
+    const val ENDED_YMD = "20221101"
+}
