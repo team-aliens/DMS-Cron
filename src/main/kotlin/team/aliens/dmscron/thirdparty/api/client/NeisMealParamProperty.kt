a862d48c98c3de7c9a2f5a698dacc3c5096dedde
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-07 19:13:19 +0900

    feat: (#5) neis param & value property

---
+++ src/main/kotlin/team/aliens/dmscron/thirdparty/api/client/NeisMealParamProperty.kt
@@ -0,0 +1,12 @@
+package team.aliens.dmscron.thirdparty.api.client
+
+object NeisMealParamProperty {
+    const val KEY = "KEY"
+    const val TYPE = "Type"
+    const val PAGE_INDEX = "pIndex"
+    const val PAGE_SIZE = "pSize"
+    const val SCHOOL_CODE = "ATPT_OFCDC_SC_CODE"
+    const val SD_SCHOOL_CODE = "SD_SCHUL_CODE"
+    const val STARTED_YMD = "MLSV_FROM_YMD"
+    const val ENDED_YMD = "MLSV_TO_YMD"
+}
