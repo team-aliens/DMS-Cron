0f219a8d38c5421cb5ae8ac91681775e49074573
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-21 22:57:38 +0900

    docs: (#5) Javadoc 설명 추가

--- src/main/kotlin/team/aliens/dmscron/school/service/NeisSchoolInfoService.kt
+++ src/main/kotlin/team/aliens/dmscron/school/service/NeisSchoolInfoService.kt
@@ -3,28 +3,40 @@ package team.aliens.dmscron.school.service
 import com.google.gson.Gson
 import org.springframework.stereotype.Service
 import team.aliens.dmscron.school.service.dto.ProcessedNeisSchoolInfoResponse
-import team.aliens.dmscron.thirdparty.api.FeignClientProperty
+import team.aliens.dmscron.thirdparty.api.client.properties.NeisRequestProperty
 import team.aliens.dmscron.thirdparty.api.client.NeisSchoolInfo
 import team.aliens.dmscron.thirdparty.api.client.dto.NeisSchoolInfoResponse
 
+/**
+ *
+ * 학교 이름과 주소로 학교기본정보 Neis API 를 호출해서 가공하는 Service
+ *
+ * @author leejeongyoon
+ * @date 2022/11/21
+ * @version 1.0.0
+ **/
 @Service
 class NeisSchoolInfoService(
     private val neisSchoolInfo: NeisSchoolInfo
 ) {
 
     fun execute(schoolName: String, schoolAddress: String): ProcessedNeisSchoolInfoResponse {
+
+        /**
+         * HTML 형식의 JSON 을 Gson 을 사용하여 오브젝트로 변환
+         **/
         val neisSchoolInfoHtml = neisSchoolInfo.getNeisSchoolInfo(
-            type = FeignClientProperty.TYPE,
-            pageIndex = FeignClientProperty.PAGE_INDEX,
-            pageSize = FeignClientProperty.PAGE_SIZE,
+            type = NeisRequestProperty.TYPE,
+            pageIndex = NeisRequestProperty.PAGE_INDEX,
+            pageSize = NeisRequestProperty.PAGE_SIZE,
             schoolName = schoolName,
             schoolAddress = schoolAddress
         )
         val neisSchoolInfoResponse = Gson().fromJson(neisSchoolInfoHtml, NeisSchoolInfoResponse::class.java)
 
         return ProcessedNeisSchoolInfoResponse(
-            sdSchoolCode = neisSchoolInfoResponse.schoolInfo[0].row[0].SD_SCHUL_CODE,
-            regionCode = neisSchoolInfoResponse.schoolInfo[0].row[0].ATPT_OFCDC_SC_CODE
+            sdSchoolCode = neisSchoolInfoResponse.schoolInfo[1].row[0].SD_SCHUL_CODE,
+            regionCode = neisSchoolInfoResponse.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE
         )
     }
 }
