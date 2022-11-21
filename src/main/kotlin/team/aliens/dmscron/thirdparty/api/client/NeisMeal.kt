e7c4a28fd1734911f223ea6ea5892a9072242756
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-21 22:57:07 +0900

    refactor: (#5) neis param property 적용

--- src/main/kotlin/team/aliens/dmscron/thirdparty/api/client/NeisMeal.kt
+++ src/main/kotlin/team/aliens/dmscron/thirdparty/api/client/NeisMeal.kt
@@ -3,19 +3,20 @@ package team.aliens.dmscron.thirdparty.api.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import team.aliens.dmscron.thirdparty.api.client.properties.NeisParamProperty
 
 @FeignClient(name = "neis-meal", url = "https://open.neis.go.kr/hub")
 interface NeisMeal {
 
     @GetMapping("/mealServiceDietInfo")
     fun getNeisMeal(
-        @RequestParam(name = NeisMealParamProperty.KEY) key: String,
-        @RequestParam(name = NeisMealParamProperty.TYPE) type: String,
-        @RequestParam(name = NeisMealParamProperty.PAGE_INDEX) pageIndex: Int,
-        @RequestParam(name = NeisMealParamProperty.PAGE_SIZE) pageSize: Int,
-        @RequestParam(name = NeisMealParamProperty.SCHOOL_CODE) sdSchoolCode: String,
-        @RequestParam(name = NeisMealParamProperty.SD_SCHOOL_CODE) schoolCode: String,
-        @RequestParam(name = NeisMealParamProperty.STARTED_YMD) startedYmd: String,
-        @RequestParam(name = NeisMealParamProperty.ENDED_YMD) endedYmd: String
+        @RequestParam(name = NeisParamProperty.KEY) key: String,
+        @RequestParam(name = NeisParamProperty.TYPE) type: String,
+        @RequestParam(name = NeisParamProperty.PAGE_INDEX) pageIndex: Int,
+        @RequestParam(name = NeisParamProperty.PAGE_SIZE) pageSize: Int,
+        @RequestParam(name = NeisParamProperty.SD_SCHOOL_CODE) sdSchoolCode: String,
+        @RequestParam(name = NeisParamProperty.REGION_CODE) regionCode: String,
+        @RequestParam(name = NeisParamProperty.STARTED_YMD) startedYmd: String,
+        @RequestParam(name = NeisParamProperty.ENDED_YMD) endedYmd: String
     ): String
 }
