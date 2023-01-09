ae695805049c8ced71c36ba7eab2ae2b1053e2ec
Jeong Yoon <dlwjddbs14@naver.com>
2023-01-09 16:27:42 +0900

    style: (#14) 패키지 변경

--- src/main/kotlin/team/aliens/dmscron/domain/school/persistence/repository/SchoolJpaRepository.kt
+++ src/main/kotlin/team/aliens/dmscron/domain/school/persistence/repository/SchoolJpaRepository.kt
@@ -1,9 +1,9 @@
-package team.aliens.dmscron.school.repository
+package team.aliens.dmscron.domain.school.persistence.repository
 
 import java.util.UUID
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import team.aliens.dmscron.school.entity.SchoolJpaEntity
+import team.aliens.dmscron.domain.school.persistence.entity.SchoolJpaEntity
 
 @Repository
 interface SchoolJpaRepository : JpaRepository<SchoolJpaEntity, UUID> {
