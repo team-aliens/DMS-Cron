74966e3dd17c5258921e1e1d5b14e8944f012860
Jeong Yoon <dlwjddbs14@naver.com>
2023-01-09 16:26:54 +0900

    chore: (#14) 기존 방식의 exception 제거

--- src/main/kotlin/team/aliens/dmscron/global/exception/InternalServerErrorException.kt
+++
@@ -1,7 +0,0 @@
-package team.aliens.dmscron.global.exception
-
-import team.aliens.dmscron.global.error.GlobalErrorCode
-
-object InternalServerErrorException : DmsCronException(
-    GlobalErrorCode.INTERNAL_SERVER_ERROR
-)
--- src/main/kotlin/team/aliens/dmscron/thirdparty/api/exception/OtherServerUnAuthorizedException.kt
+++
@@ -1,8 +0,0 @@
-package team.aliens.dmscron.thirdparty.api.exception
-
-import team.aliens.dmscron.global.exception.DmsCronException
-import team.aliens.dmscron.thirdparty.api.error.FeignClientErrorCode
-
-object OtherServerUnAuthorizedException : DmsCronException(
-    FeignClientErrorCode.OTHER_SERVER_UN_AUTHORIZED
-)
