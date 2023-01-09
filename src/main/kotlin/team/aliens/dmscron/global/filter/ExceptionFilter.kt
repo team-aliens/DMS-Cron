94b2f29ed1d26a56a6c8dbd91cda7ce57caec66e
Jeong Yoon <dlwjddbs14@naver.com>
2023-01-09 16:31:41 +0900

    fix: (#14) 바뀐 exception 적용

--- src/main/kotlin/team/aliens/dmscron/global/filter/ExceptionFilter.kt
+++ src/main/kotlin/team/aliens/dmscron/global/filter/ExceptionFilter.kt
@@ -3,15 +3,14 @@ package team.aliens.dmscron.global.filter
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.MediaType
 import org.springframework.web.filter.OncePerRequestFilter
-import team.aliens.dmscron.global.error.ErrorProperty
 import team.aliens.dmscron.global.error.ErrorResponse
 import team.aliens.dmscron.global.exception.DmsCronException
-import team.aliens.dmscron.global.exception.InternalServerErrorException
 import java.lang.Exception
 import java.nio.charset.StandardCharsets
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import team.aliens.dmscron.global.exception.GlobalExceptions
 
 class ExceptionFilter(
     private val objectMapper: ObjectMapper
@@ -26,19 +25,23 @@ class ExceptionFilter(
             filterChain.doFilter(request, response)
         } catch (e: Exception) {
             when (e) {
-                is DmsCronException -> errorToJson((e.cause as DmsCronException).errorProperty, response)
+                is DmsCronException -> errorToJson((e.cause as DmsCronException), response)
                 else -> {
-                    errorToJson(InternalServerErrorException.errorProperty, response)
+                    errorToJson(GlobalExceptions.InternalServerError(), response)
                     e.printStackTrace()
                 }
             }
         }
     }
 
-    private fun errorToJson(errorProperty: ErrorProperty, response: HttpServletResponse) {
-        response.status = errorProperty.status().value()
+    private fun errorToJson(exception: DmsCronException, response: HttpServletResponse) {
+        response.status = exception.status
         response.characterEncoding = StandardCharsets.UTF_8.name()
         response.contentType = MediaType.APPLICATION_JSON_VALUE
-        response.writer.write(objectMapper.writeValueAsString(ErrorResponse.of(errorProperty)))
+        response.writer.write(
+            objectMapper.writeValueAsString(
+                ErrorResponse.of(exception)
+            )
+        )
     }
 }
