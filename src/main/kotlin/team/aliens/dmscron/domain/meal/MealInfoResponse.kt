4a578197b009c4ed011d921336bc9f9ac206902e
Jeong Yoon <dlwjddbs14@naver.com>
2023-01-09 16:32:11 +0900

    style: (#14) 클래스 이름 변경

--- src/main/kotlin/team/aliens/dmscron/domain/meal/MealInfoResponse.kt
+++ src/main/kotlin/team/aliens/dmscron/domain/meal/MealInfoResponse.kt
@@ -1,8 +1,8 @@
-package team.aliens.dmscron.meal.service.dto
+package team.aliens.dmscron.domain.meal
 
 import java.time.LocalDate
 
-data class ProcessedMealResponse(
+data class MealInfoResponse(
     val mealDate: LocalDate,
     val breakfast: String,
     val lunch: String,
---
+++ src/main/kotlin/team/aliens/dmscron/domain/school/SchoolInfoResponse.kt
@@ -0,0 +1,6 @@
+package team.aliens.dmscron.domain.school
+
+data class SchoolInfoResponse(
+    val sdSchoolCode: String,
+    val regionCode: String
+)
